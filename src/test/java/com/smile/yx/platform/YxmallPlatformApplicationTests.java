fd04345ae159629ee4b47e60930c516fdde00da5
1248847966@qq.com <1248847966@qq.com>
2018-09-26 09:59:28 +0800

    写了个测试方法

--- src/test/java/com/smile/yx/platform/YxmallPlatformApplicationTests.java
+++ src/test/java/com/smile/yx/platform/YxmallPlatformApplicationTests.java
@@ -4,18 +4,30 @@ import com.github.jsonzou.jmockdata.JMockData;
 import com.github.jsonzou.jmockdata.MockConfig;
 import com.smile.yx.platform.entity.SysUser;
 import com.smile.yx.platform.mapper.SysUserMapper;
+import com.smile.yx.platform.redis.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ComponentScan("com.smile.yx.platform.redis")
 public class YxmallPlatformApplicationTests {
 
     @Autowired
     private SysUserMapper sysUserMapper;
+    @Autowired
+    private RedisService redisService;
+
+
+    @Autowired
+    private RedisConnectionFactory factory;
+
+
 
     @Test
     public void contextLoads() {
@@ -28,6 +40,11 @@ public class YxmallPlatformApplicationTests {
             SysUser user = JMockData.mock(SysUser.class,mockConfig);
             sysUserMapper.insert(user);
         }
+    }
+    @Test
+    public void redisTest(){
+        redisService.set("wangyajun",178,(long)10);
+        System.out.print(redisService.get("wangyajun")+"======================");
     }
 
 }
