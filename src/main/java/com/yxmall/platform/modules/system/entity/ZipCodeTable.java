f80d184cdbb6853951c0dd63b6b614461e9a0deb
1248847966@qq.com <1248847966@qq.com>
2018-11-20 16:32:09 +0800

    sqlserver zipcode表对应的实体类

---
+++ src/main/java/com/yxmall/platform/modules/system/entity/ZipCodeTable.java
@@ -0,0 +1,13 @@
+package com.yxmall.platform.modules.system.entity;
+
+import lombok.Data;
+
+/**
+ * Created by Dell on 28/10/2018.
+ */
+@Data
+public class ZipCodeTable {
+    private String stateCode;
+    private String lowestZip;
+    private String highestZip;
+}
